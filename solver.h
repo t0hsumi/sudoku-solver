4b6d93f69fee176755077d6a4c6b75107dd4c245
tohsumi <tohsumi@student.42tokyo.jp>
2021-12-14 12:07:56 +0900

    Add func prototype

--- solver.h
+++ solver.h
@@ -5,7 +5,9 @@
 
 #define rep(i,n) for(int i=0;i<(int)(n);i++)
 
-void init(char map[9][9]);
-void output(char map[9][9]);
+void	init(char map[9][9]);
+void	output(char map[9][9]);
+int		checker(char map[9][9], int x, int y);
+int		solve(char map[9][9], int x, int y);
 
 #endif
