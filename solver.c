a464d6bdff4116a97493f879e3ea34e9454e4f1c
tohsumi <tohsumi@student.42tokyo.jp>
2022-01-02 18:11:57 +0900

    Del printf debug and improve readability

--- solver.c
+++ solver.c
@@ -1,45 +1,30 @@
 #include "solver.h"
 
 int solve(char map[9][9], int x, int y, int n){
-	int res;
+	int fixed = 0;
 
-	if (x == 0 && y == 9){
-		output(map);
-		return 0;
-	}
-	if (map[y][x] >= '1' && map[y][x] <= '9'){
-		if (x == 8){
-			for (int i = 1; i < 10; i++)
-				solve(map, 0, y+1, i);
-			/* if (solve(map, 0, y+1)) */
-			/* 	res = 1; */
-		}
-		else{
-			for (int i = 1; i < 10; i++)
-				solve(map, x+1, y, i);
-			/* if (solve(map, x+1, y)) */
-			/* 	res = 1; */
-		}
-	}
+	if (map[y][x] == n + '0')
+		fixed = 1;
 	else{
-		printf("map[%c][%c] = %d\n", y+'0', x+'0', n);
+		if (map[y][x] != '0')
+			return 0;
 		map[y][x] = n + '0';
-		if (checker(map, x, y)){
-			if (x == 8){
-				for (int i = 1; i < 10; i++)
-					solve(map, 0, y+1, i);
-				/* if (solve(map, 0, y+1)) */
-				/* 	res = 1; */
-			}
-			else{
-				for (int i = 1; i < 10; i++)
-					solve(map, x+1, y, i);
-				/* if (solve(map, x+1, y)) */
-				/* 	res = 1; */
-			}
+		if (!checker(map, x, y)){
+			map[y][x] = '0';
+			return 0;
 		}
-		map[y][x] = 0;
-		res = 0;
 	}
-	return res;
+
+	int next_x = ((x == 8) ? 0 : x+1);
+	int next_y = ((x == 8) ? y+1 : y);
+	if (next_x == 0 && next_y == 9)
+		output(map);
+	else{
+		for (int i = 1; i < 10; i++){
+			solve(map, next_x, next_y, i);
+		}
+	}
+	if (fixed == 0)
+		map[x][y] = '0';
+	return 1;
 }
